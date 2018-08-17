9bdaee55e870fad2478659dddb0d71fd585841cb
privateairjet <john_edel_tamani@dlsu.edu.ph>
2018-08-17 14:28:20 +0800

    updated

---
+++ green-cat/resources/views/greencat/signup.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+		<link rel="stylesheet" href="https://v40.pingendo.com/assets/4.0.0/default/theme.css" type="text/css">
+	</head>
+	<body>
+		<div class="py-5" style="background-image: url(&quot;https://pingendo.github.io/templates/sections/assets/form_red.jpg&quot;);">
+			<div class="container">
+				<div class="row">
+					<div class="align-self-center col-md-6 text-white">
+						<h1 class="text-center text-md-left display-3">Sign up</h1>
+						<p class="lead">Join and meet hundreds of Lasallians!</p>
+					</div>
+					<div class="col-md-6">
+						<div class="card">
+							<div class="card-body p-5">
+								<h3 class="pb-3">Create an Account</h3>
+								<div class="form-group">
+									<label>Username</label>
+									<input class="form-control" name="username" placeholder="Username" type="text" required> 
+								</div>
+								<div class="form-group">
+									<label>ID Number</label>
+									<input type="number" class="form-control" name="dlsuid" placeholder="e.g. 11412345" required> 
+								</div>
+								<div class="form-group">
+									<label>Email Address</label>
+									<input type="email" class="form-control" name="email" placeholder="e.g. juan_delacruz@dlsu.edu.ph" required> 
+								</div>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Password</label>
+									<input type="password" name="password" class="form-control" required> 
+								</div>
+								<button type="submit" name="register" class="btn mt-2 btn-outline-dark">Sign up
+								</button>
+								<br><br>
+                Already have an existing account? <a href={{route('login@index')}}>Log in here.</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+		<img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16">
+	</body>
+</html>
